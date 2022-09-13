649a23be731c23943c832280a8fc6c45df54e1fe
marsv <marijasverdl@gmail.com>
2022-09-13 12:35:46 +0200

    KQ7UO35XHBJ

--- js_hw/js_1.js
+++ js_hw/js_1.js
@@ -1,5 +1,5 @@
 var Kvat = 193 // kol-vo kilowat elektroenergii
 var KvatPrice = .21 // stoimost 1 kv elektroenergii
-var TransitPrice = 10.45 // stoimost uslugi dostawki
-var FinalTax = Kvat * KvatPrice + 10.45 // summa k oplate
+var TransitPrice = 10.45 // stoimost uslugi dostawki 
+var FinalTax = Kvat * KvatPrice + 10.45 // summa k oplate  
 prompt ('summa k oplate sostawlaet', FinalTax)
